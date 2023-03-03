8c7cd0c006542a7f69def8aec39fe6781d00f78c
Link19224 <124432749+Link19224@users.noreply.github.com>
2023-03-04 01:27:45 +0300

    4 модуль

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+  for (let element of friends) {
+    let li = document.createElement('li');
+    li.innerHTML = `${element.firstName} ${element.lastName}`;
+    ul.appendChild(li);
+  }
+  return ul;
 }
