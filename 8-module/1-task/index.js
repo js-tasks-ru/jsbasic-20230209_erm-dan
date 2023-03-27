d087ebf2edb44139da4d7e505d49dc1690dcc9be
Link19224 <124432749+Link19224@users.noreply.github.com>
2023-03-28 01:28:17 +0300

    7-4, 8-1,8-2,8-3(не рабочее)

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -39,6 +39,45 @@ export default class CartIcon {
   }
 
   updatePosition() {
-    // ваш код ...
+
+    if (!this.elem.offsetHeight) {return;} 
+    if (!this.initialTopCoord) {
+      this.initialTopCoord = this.elem.getBoundingClientRect().top + window.pageYOffset;
+      console.log(this.initialTopCoord);
+    }
+
+    if (document.documentElement.clientWidth <= 767) {
+      this.resetPosition();
+      return;
+    }
+
+    let isHeaderCartScrolled = window.pageYOffset > this.initialTopCoord;
+
+    if (isHeaderCartScrolled) {
+      this.fixPosition();
+    } else {
+      this.resetPosition();
+    }
+  }
+
+  fixPosition() {
+    Object.assign(this.elem.style, {
+      position: 'fixed',
+      top: '50px',
+      zIndex: 1e3,
+      left: Math.min(
+        document.querySelector('.container').getBoundingClientRect().right + 20,
+        document.documentElement.clientWidth - this.elem.offsetWidth - 10
+      ) + 'px'
+    });
+  }
+
+  resetPosition() {
+    Object.assign(this.elem.style, {
+      position: '',
+      top: '',
+      left: '',
+      zIndex: ''
+    });
   }
 }
--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -5,5 +5,37 @@ export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+    this.render();
+  }
+
+  render() {
+    this.elem = createElement(`<div class="products-grid">
+    <div class="products-grid__inner">
+    </div>
+  </div>`);
+
+    this.createEl();
+  }
+
+  createEl() {
+    this.sub('inner').innerHTML = '';
+    for (let elements of this.products) {
+      if (this.filters.noNuts && elements.nuts) {continue;}
+      if (this.filters.vegeterianOnly && !elements.vegeterian) {continue;}
+      if (this.filters.maxSpiciness !== undefined && elements.spiciness > this.filters.maxSpiciness) {continue;}
+      if (this.filters.category && elements.category != this.filters.category) {continue;}
+      let card = new ProductCard(elements);
+      this.sub('inner').append(card.elem);
+
+    }
+  }
+
+  updateFilter(filters) {
+    Object.assign(this.filters, filters);
+    this.createEl();
+  }
+
+  sub(ref) {
+    return this.elem.querySelector(`.products-grid__${ref}`);
   }
 }
